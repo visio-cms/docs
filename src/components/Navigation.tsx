59ab19a4010aacacc1d89f9e48e6c373c581208d
Adolphus Okrah <adolphus.okrah@retraced.com>
2024-09-13 13:32:46 +0200

    fix: removed un wanted menu

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -289,9 +289,7 @@ export function Navigation(props: React.ComponentPropsWithoutRef<'nav'>) {
   return (
     <nav {...props}>
       <ul role="list">
-        <TopLevelNavItem href="/">API</TopLevelNavItem>
-        <TopLevelNavItem href="#">Documentation</TopLevelNavItem>
-        <TopLevelNavItem href="#">Support</TopLevelNavItem>
+   
         {navigation.map((group, groupIndex) => (
           <NavigationGroup
             key={group.title}
@@ -299,11 +297,7 @@ export function Navigation(props: React.ComponentPropsWithoutRef<'nav'>) {
             className={groupIndex === 0 ? 'md:mt-0' : ''}
           />
         ))}
-        <li className="sticky bottom-0 z-10 mt-6 min-[416px]:hidden">
-          <Button href="#" variant="filled" className="w-full">
-            Sign in
-          </Button>
-        </li>
+        
       </ul>
     </nav>
   )
